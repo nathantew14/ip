90b9e3116e92688f330cc7ce7b02f8fc14d5cb8c
nathantew14 <nathantew14@gmail.com>
2025-08-29 21:45:00 +0800

    Move command parsing logic to CommandParser

--- src/main/java/CommandParser.java
+++ src/main/java/CommandParser.java
@@ -1,5 +1,14 @@
 public class CommandParser {
     public static CommandType parseCommand(String input) {
-        return CommandType.fromInput(input);
+        String lowerInput = input.toLowerCase();
+        for (CommandType type : CommandType.values()) {
+            if (type != CommandType.UNKNOWN && lowerInput.startsWith(type.getPrefix())) {
+                return type;
+            }
+            if (type == CommandType.LIST_TODOS && lowerInput.equals(type.getPrefix())) {
+                return type;
+            }
+        }
+        return CommandType.UNKNOWN;
     }
 }
--- src/main/java/CommandType.java
+++ src/main/java/CommandType.java
@@ -16,17 +16,4 @@ public enum CommandType {
     public String getPrefix() {
         return prefix;
     }
-
-    public static CommandType fromInput(String input) {
-        String lowerInput = input.toLowerCase();
-        for (CommandType type : values()) {
-            if (type != UNKNOWN && lowerInput.startsWith(type.getPrefix())) {
-                return type;
-            }
-            if (type == LIST_TODOS && lowerInput.equals(type.getPrefix())) {
-                return type;
-            }
-        }
-        return UNKNOWN;
-    }
 }
